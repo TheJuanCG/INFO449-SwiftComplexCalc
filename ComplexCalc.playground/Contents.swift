658d13dcbe1fb317c2e3348270963b8a2d9826fc
TheJuanCG <114948315+TheJuanCG@users.noreply.github.com>
2025-04-08 17:06:33 -0700

    Finished first half of problems

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -28,6 +28,54 @@ print("Welcome back to the UW Calculator")
 //: IMPORTANT: If any tests are commented out, you will be graded a zero (0)! You should never be in the habit of eliminating tests to make the code pass.
 //:
 class Calculator {
+    // Lets implement the add function first
+    
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs;
+    }
+    
+    func add(_ args: [Int]) -> Int {
+        var sum = 0
+        for n in args {
+            sum += n
+        }
+        
+        return sum
+    }
+
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs;
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs;
+    }
+    
+    func multiply(_ args: [Int]) -> Int {
+        var prod = 0
+        for n in args {
+            prod *= n
+        }
+        
+        return prod
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs;
+    }
+    
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs);
+    }
+    
+    func count(_ args: [Int]) -> Int {
+        return args.count;
+    }
+    
+    func avg(_ args: [Int]) -> Int {
+        return add(args) / args.count
+    }
+    
 }
 
 //: Don't change the name of this object (`calc`); it's used in all the tests.
@@ -52,7 +100,7 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
-calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rjs) + (lhs * rhs) }) == 35
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
     // This style is one way of writing an anonymous function
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
     // This is the second, more terse, style; either works
